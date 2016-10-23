7b794f09c5a36b2ac2ba78e9b2f9a87f6dfb748c
efthemiosprime <efthemios.suyat@gmail.com>
2016-10-23 03:35:44 -0400

    adding create post

--- Hueman/CreatePostViewController.swift
+++ Hueman/CreatePostViewController.swift
@@ -7,11 +7,28 @@
 //
 
 import UIKit
+import AssetsLibrary
+import Photos
+import MobileCoreServices
 
-class CreatePostViewController: UIViewController {
+class CreatePostViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var inputBackground: UIView!
+    @IBOutlet weak var postInput: UITextView!
+    @IBOutlet weak var headerLabel: UILabel!
     @IBOutlet weak var icon: UIImageView!
+    @IBOutlet weak var topic: UIView!
+    @IBOutlet weak var cameraButton: UIButton!
+    @IBOutlet weak var filterButton: UIButton!
+    @IBOutlet weak var postButton: UIButton!
+    
+    @IBOutlet weak var wanderlustButton: UIButton!
+    @IBOutlet weak var plateButton: UIButton!
+    @IBOutlet weak var relationshopButton: UIButton!
+    @IBOutlet weak var healthButton: UIButton!
+    @IBOutlet weak var hustleButton: UIButton!
+    @IBOutlet weak var raylightButton: UIButton!
+
     
     @IBAction func backButton(sender: AnyObject) {
         self.dismissViewControllerAnimated(true, completion: {})
@@ -20,16 +37,89 @@ class CreatePostViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        self.view.backgroundColor = UIColor.UIColorFromRGB(0x93648D)
+        
+        showTopic(true)
+        
+        postInput.delegate = self
         self.navigationController?.navigationBar.barTintColor = UIColor.UIColorFromRGB(0x93648D)
 
-        inputBackground.backgroundColor = UIColor.UIColorFromRGB(0x93648D)
         inputBackground.layer.borderWidth = 2
         inputBackground.layer.borderColor = UIColor.whiteColor().CGColor
+        
+        self.view.backgroundColor = UIColor.UIColorFromRGB(0xe2563b)
+        inputBackground.backgroundColor = UIColor.UIColorFromRGB(0xe2563b)
+        icon.image = UIImage(named: "relationship-icon.png")
+        
+        let buttons: [UIButton] = [wanderlustButton, plateButton, relationshopButton, healthButton, hustleButton, raylightButton]
+        
+        for btn in buttons {
+            btn.addTarget(self, action: #selector(CreatePostViewController.topicChangedAction(_:)), forControlEvents: .TouchUpInside)
+        }
+        
+        cameraButton.addTarget(self, action: #selector(CreatePostViewController.openPhotoLibrary), forControlEvents: .TouchUpInside)
+        filterButton.addTarget(self, action: #selector(CreatePostViewController.showTopicAction), forControlEvents: .TouchUpInside)
+
+    }
+    
+    func showTopic(show: Bool) {
+        topic.hidden = show
 
     }
 
+    func topicChangedAction(sender: UIButton!) {
+        print(sender.tag)
+        if sender.tag == 0 {
+            self.view.backgroundColor = UIColor.UIColorFromRGB(0x34b5d4)
+            inputBackground.backgroundColor = UIColor.UIColorFromRGB(0x34b5d4)
+            icon.image = UIImage(named: "wanderlust-icon.png")
+        }
+        
+        if sender.tag == 1 {
+            self.view.backgroundColor = UIColor.UIColorFromRGB(0xF49445)
+            inputBackground.backgroundColor = UIColor.UIColorFromRGB(0xF49445)
+            icon.image = UIImage(named: "plate-icon.png")
+        }
+        
+        if sender.tag == 2 {
+            self.view.backgroundColor = UIColor.UIColorFromRGB(0xe2563b)
+            inputBackground.backgroundColor = UIColor.UIColorFromRGB(0xe2563b)
+            icon.image = UIImage(named: "relationship-icon.png")
+        }
+        
+        if sender.tag == 3 {
+            self.view.backgroundColor = UIColor.UIColorFromRGB(0x7BC8A4)
+            inputBackground.backgroundColor = UIColor.UIColorFromRGB(0x7BC8A4)
+            icon.image = UIImage(named: "health-icon.png")
+        }
+        
+        if sender.tag == 4 {
+            self.view.backgroundColor = UIColor.UIColorFromRGB(0x93648D)
+            inputBackground.backgroundColor = UIColor.UIColorFromRGB(0x93648D)
+            icon.image = UIImage(named: "hustle-icon.png")
+        }
+        
+        if sender.tag == 5 {
+            self.view.backgroundColor = UIColor.UIColorFromRGB(0xEACD53)
+            inputBackground.backgroundColor = UIColor.UIColorFromRGB(0xEACD53)
+            icon.image = UIImage(named: "ray-light-icon.png")
+        }
+        
+        showTopic(true)
+    }
+
+    func showTopicAction() {
+        showTopic(false)
+    }
+    
+    func openPhotoLibrary() {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.PhotoLibrary) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+            imagePicker.allowsEditing = true
+            self.presentViewController(imagePicker, animated: true, completion: nil)
+        }
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -47,4 +137,21 @@ class CreatePostViewController: UIViewController {
     }
     */
 
+}
+
+extension CreatePostViewController: UITextViewDelegate {
+    
+    
+    func textViewShouldBeginEditing(textView: UITextView) -> Bool {
+        textView.text = ""
+        return true
+    }
+    func textViewDidChange(textView: UITextView) {
+        headerLabel.text = "\(250 - textView.text.characters.count)/250 characters left"
+        
+    }
+    
+    func textView(textView: UITextView, shouldChangeTextInRange range: NSRange, replacementText text: String) -> Bool {
+        return textView.text.characters.count + (text.characters.count - range.length) <= 250
+    }
 }
