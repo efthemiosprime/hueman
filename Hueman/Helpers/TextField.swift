c609851e1fe6ef67a1c99e28530db2931c1fd672
efthemiosprime <efthemios.suyat@gmail.com>
2016-12-11 23:26:26 -0500

    bindable textfield inset

---
+++ Hueman/Helpers/TextField.swift
@@ -0,0 +1,27 @@
+//
+//  TextField.swift
+//  Hueman
+//
+//  Created by Efthemios Prime on 12/11/16.
+//  Copyright © 2016 Efthemios Prime. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class TextField: UITextField {
+    @IBInspectable var insetX: CGFloat = 0
+    @IBInspectable var insetY: CGFloat = 0
+    
+    // placeholder position
+    override func textRectForBounds(bounds: CGRect) -> CGRect {
+        return CGRectInset(bounds , insetX , insetY)
+    }
+    
+    // text position
+    override func editingRectForBounds(bounds: CGRect) -> CGRect {
+        return CGRectInset(bounds , insetX , insetY)
+    }
+    
+
+}
